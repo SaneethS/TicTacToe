7d50930d6fee16189a8276d132dbedd9efabc0d6
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-26 09:54:37 +0530

    Created a empty board game

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -1,5 +1,18 @@
 package com.tictactoe;
 
 public class TicTacToe {
-
+	private char[] board = new char[10];
+	
+	public void createBoard() {
+		for(int i=1;i<board.length;i++) {
+			board[i] = ' ';
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to board game");
+		TicTacToe ticTacToe = new TicTacToe(); 
+		ticTacToe.createBoard();
+	}
 }
