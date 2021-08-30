dd8f77aa0e4edc11a14ef96faccb5916a54adf7d
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-30 11:32:25 +0530

    UC7 has been created where it is checked whether the player or computer won or tie and changed turn

---
+++ src/com/tictactoe/Outcome.java
@@ -0,0 +1,91 @@
+package com.tictactoe;
+
+/**
+ * This class is created for UC7 where we check if the play or com has won
+ * or is it a tie
+ * @author saneeths
+ *
+ */
+public class Outcome {
+	/**
+	 * @return this is a method with returntype char where each line of board is checked
+	 * where its equal or not
+	 */
+	public static char winCheck() {
+		
+		//@parameter check the line which win
+		String winLine = null;
+		
+		for(int w=1;w<9;w++) {
+			switch(w) {
+			case 1:
+				winLine = ""+TicTacToeGame.board[1]+TicTacToeGame.board[2]+TicTacToeGame.board[3];
+				break;
+			case 2:
+				winLine = ""+TicTacToeGame.board[4]+TicTacToeGame.board[5]+TicTacToeGame.board[6];
+				break;
+			case 3:
+				winLine = ""+TicTacToeGame.board[7]+TicTacToeGame.board[8]+TicTacToeGame.board[9];
+				break;
+			case 4:
+				winLine = ""+TicTacToeGame.board[1]+TicTacToeGame.board[4]+TicTacToeGame.board[7];
+				break;
+			case 5:
+				winLine = ""+TicTacToeGame.board[2]+TicTacToeGame.board[5]+TicTacToeGame.board[8];
+				break;
+			case 6:
+				winLine = ""+TicTacToeGame.board[3]+TicTacToeGame.board[6]+TicTacToeGame.board[9];
+				break;
+			case 7:
+				winLine = ""+TicTacToeGame.board[1]+TicTacToeGame.board[5]+TicTacToeGame.board[9];
+				break;
+			case 8:
+				winLine = ""+TicTacToeGame.board[3]+TicTacToeGame.board[5]+TicTacToeGame.board[7];
+				break;
+			}
+			 if(winLine.equals("XXX"))
+				 return 'X';
+			 else if(winLine.equals("OOO"))
+				 return 'O';
+		}
+		
+		int i ;
+		
+		for(i=1;i<=9;i++)
+			if(TicTacToeGame.board[i] == ' ')
+				break;
+		
+		if(i == 9)
+			return 'D';
+		else
+			return 'N';
+	}
+	
+	/**
+	 * @method this is the method which is used to check whether the player or com has won
+	 * or is it tie or change the turn between two
+	 */
+	public static void winTieResult() {
+		char win = winCheck();
+		
+		if(win == TicTacToeGame.playerChoice) {
+			System.out.println("You have won the game");
+		}
+		else if(win == TicTacToeGame.computerChoice) {
+			System.out.println("Computer have won the game");
+		}
+		else if(win == 'D') {
+			System.out.println("Game is a tie");
+		}
+		else {
+			if(TicTacToeGame.turn == "player") {
+				TicTacToeGame.turn = "computer";
+				System.out.println("Computer turn to play");
+			}
+			else {
+				TicTacToeGame.turn = "player";
+				System.out.println("Your turn to play");
+			}
+		}
+	}
+}
